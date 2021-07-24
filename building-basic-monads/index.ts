afbae7ad1c6ad53deff66292a6e0359c1c723506
kamilzielinskidev <kamilzielinskidev@gmail.com>
2021-07-24 18:56:19 +0200

    add maybe monad

--- building-basic-monads/index.ts
+++ building-basic-monads/index.ts
@@ -11,6 +11,15 @@ const List = <T>(x: T[]) => ({
   map: <R>(fn: LambdaFn<T, R>) => List(x.map((y) => fn(y))),
   chain: <R>(fn: LambdaFn<T[], R>) => fn(x),
   concat: (arr: T | T[]) => List(x.concat(arr)),
-  head: () => (x.length > 0 ? List([x[0]]) : List([] as T[])),
+  head: () => (x.length > 0 ? x[0] : List([] as T[])),
   tail: () => (x.length > 0 ? List(x.slice(1)) : List([] as T[])),
+});
+
+const Maybe = <T>(x: T) => ({
+  emit: () => x,
+  map: <R>(fn: LambdaFn<T, R>) =>
+    x === null || x === undefined ? Maybe(x) : Maybe(fn(x)),
+  chain: <R>(fn: LambdaFn<T, R>) => (x === null || x === undefined ? x : fn(x)),
+  fork: <R, U>(errFn: LambdaFn<T, R>, passFn: LambdaFn<T, U>) =>
+    x === null || x === undefined ? errFn(x) : passFn(x),
 });
